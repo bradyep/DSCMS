e5af049ae6fff9548a976fb2b3e49509a4c4ad79
Brady Prigge <syntonicstudios@gmail.com>
2016-07-14 20:34:06 -0500

    Added Includes to DSCMS controller. Fixed up Layouts and Templates admin.

--- src/DSCMS/Controllers/DSCMSController.cs
+++ src/DSCMS/Controllers/DSCMSController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DSCMS.Data;
@@ -27,54 +28,56 @@ namespace DSCMS.Controllers
       ViewData["ContentTypeName"] = contentTypeName;
       ViewData["ContentUrl"] = contentUrl;
 
-      ContentType contentType = _context.ContentTypes.Where(ct => ct.Name == contentTypeName).FirstOrDefault();
+      ContentType contentType = _context.ContentTypes
+        .Include(ct => ct.ContentTypeItems)
+        .Where(ct => ct.Name == contentTypeName).FirstOrDefault();
       if (contentType == null) return NotFound();
 
-      if (contentUrl.Trim() != "")
-      { // Content was requested
-        content = _context.Contents.Where(c => c.UrlToDisplay == contentUrl && c.ContentTypeId == contentType.ContentTypeId).FirstOrDefault();
+      if (contentUrl.Trim() != "") // Content was requested
+      { 
+        content = _context.Contents
+          .Include(c => c.CreatedByUser)
+          .Include(c => c.LastUpdatedByUser)
+          .Include(c => c.ContentItems)
+          .Where(c => c.UrlToDisplay == contentUrl && c.ContentTypeId == contentType.ContentTypeId)
+          .FirstOrDefault();
         if (content == null) return NotFound();
         content.ContentType = contentType;
-        content.CreatedByUser = _context.Users.Where(u => u.UserId == content.CreatedBy).FirstOrDefault();
-        content.LastUpdatedByUser = _context.Users.Where(u => u.UserId == content.LastUpdatedBy).FirstOrDefault();
         ViewData["Title"] = content.Title ?? "Title";
-        template = _context.Templates.Where(t => t.TemplateId == content.TemplateId).FirstOrDefault();
-        // Handle ContentItems
-        content.ContentType.ContentTypeItems = _context.ContentTypeItems.Where(cti => cti.ContentTypeId == contentType.ContentTypeId).ToList();
-        content.ContentItems = _context.ContentItems
-            .Where(ci => content.ContentType.ContentTypeItems.Select(cti => cti.ContentTypeItemId)
-            .Contains(ci.ContentTypeItemId) && ci.ContentId == content.ContentId)
-            .ToList();
+        template = _context.Templates
+          .Include(t => t.Layout)
+          .Where(t => t.TemplateId == content.TemplateId).FirstOrDefault();
       }
       else // ContentType was requested
       {
         ViewData["Title"] = contentType.Title ?? "Title";
-        template = _context.Templates.Where(t => t.TemplateId == contentType.TemplateId).FirstOrDefault();
-        contentType.ContentTypeItems = _context.ContentTypeItems.Where(cti => cti.ContentTypeId == contentType.ContentTypeId).ToList();
+        template = _context.Templates
+          .Include(t => t.Layout)
+          .Where(t => t.TemplateId == contentType.TemplateId).FirstOrDefault();
         // Handle paging
         int pageValue = 0;
         Int32.TryParse(page, out pageValue);
         ViewData["Page"] = pageValue;
         // Handle associated Contents
         if (pageValue < 1 || contentType.ItemsPerPage < 1) // Get all Contents
-          contentType.Contents = _context.Contents.Where(c => c.ContentTypeId == contentType.ContentTypeId).ToList();
+          contentType.Contents = _context.Contents
+            .Include(c => c.CreatedByUser)
+            .Include(c => c.LastUpdatedByUser)
+            .Include(c => c.ContentItems)
+            .Where(c => c.ContentTypeId == contentType.ContentTypeId).ToList();
         else // Get Contents based on page
-          contentType.Contents = _context.Contents.Where(c => c.ContentTypeId == contentType.ContentTypeId)
+          contentType.Contents = _context.Contents
+            .Include(c => c.CreatedByUser)
+            .Include(c => c.LastUpdatedByUser)
+            .Include(c => c.ContentItems)
+            .Where(c => c.ContentTypeId == contentType.ContentTypeId)
             .Skip((pageValue - 1) * contentType.ItemsPerPage)
             .Take(contentType.ItemsPerPage)
             .ToList();
-        foreach (Content contentItem in contentType.Contents)
-        {
-          contentItem.CreatedByUser = _context.Users.Where(u => u.UserId == contentItem.CreatedBy).FirstOrDefault();
-          contentItem.LastUpdatedByUser = _context.Users.Where(u => u.UserId == contentItem.LastUpdatedBy).FirstOrDefault();
-          contentItem.ContentItems = _context.ContentItems
-              .Where(ci => contentType.ContentTypeItems.Select(cti => cti.ContentTypeItemId).Contains(ci.ContentTypeItemId) && ci.ContentId == contentItem.ContentId)
-              .ToList();
-        }
       }
 
-      Layout layout = _context.Layouts.Where(l => l.LayoutId == template.LayoutId).FirstOrDefault();
-      ViewData["Layout"] = layout.FileLocation ?? "_Layout";
+      // Layout layout = _context.Layouts.Where(l => l.LayoutId == template.LayoutId).FirstOrDefault();
+      ViewData["Layout"] = template.Layout.FileLocation ?? "_Layout";
 
       string viewLocationToUse = template.FileLocation ?? "/Views/Home/Index.cshtml";
 
--- src/DSCMS/Controllers/TemplatesController.cs
+++ src/DSCMS/Controllers/TemplatesController.cs
@@ -11,148 +11,149 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace DSCMS.Controllers
 {
-    [Authorize]
-    public class TemplatesController : Controller
+  [Authorize]
+  public class TemplatesController : Controller
+  {
+    private readonly ApplicationDbContext _context;
+
+    public TemplatesController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
-
-        public TemplatesController(ApplicationDbContext context)
-        {
-            _context = context;    
-        }
-
-        // GET: Templates
-        public async Task<IActionResult> Index()
-        {
-            var applicationDbContext = _context.Templates.Include(t => t.Layout);
-            return View(await applicationDbContext.ToListAsync());
-        }
-
-        // GET: Templates/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var template = await _context.Templates.SingleOrDefaultAsync(m => m.TemplateId == id);
-            if (template == null)
-            {
-                return NotFound();
-            }
-
-            return View(template);
-        }
-
-        // GET: Templates/Create
-        public IActionResult Create()
-        {
-            ViewData["LayoutId"] = new SelectList(_context.Layouts, "LayoutId", "LayoutId");
-            return View();
-        }
-
-        // POST: Templates/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("TemplateId,FileContents,FileLocation,LayoutId,Name,IsForContentType")] Template template)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(template);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            ViewData["LayoutId"] = new SelectList(_context.Layouts, "LayoutId", "LayoutId", template.LayoutId);
-            return View(template);
-        }
-
-        // GET: Templates/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var template = await _context.Templates.SingleOrDefaultAsync(m => m.TemplateId == id);
-            if (template == null)
-            {
-                return NotFound();
-            }
-            ViewData["LayoutId"] = new SelectList(_context.Layouts, "LayoutId", "LayoutId", template.LayoutId);
-            return View(template);
-        }
-
-        // POST: Templates/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("TemplateId,FileContents,FileLocation,LayoutId,Name,IsForContentType")] Template template)
-        {
-            if (id != template.TemplateId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(template);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TemplateExists(template.TemplateId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction("Index");
-            }
-            ViewData["LayoutId"] = new SelectList(_context.Layouts, "LayoutId", "LayoutId", template.LayoutId);
-            return View(template);
-        }
-
-        // GET: Templates/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var template = await _context.Templates.SingleOrDefaultAsync(m => m.TemplateId == id);
-            if (template == null)
-            {
-                return NotFound();
-            }
-
-            return View(template);
-        }
-
-        // POST: Templates/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var template = await _context.Templates.SingleOrDefaultAsync(m => m.TemplateId == id);
-            _context.Templates.Remove(template);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
-
-        private bool TemplateExists(int id)
-        {
-            return _context.Templates.Any(e => e.TemplateId == id);
-        }
+      _context = context;
     }
+
+    // GET: Templates
+    public async Task<IActionResult> Index()
+    {
+      var applicationDbContext = _context.Templates.Include(t => t.Layout);
+      return View(await applicationDbContext.ToListAsync());
+    }
+
+    // GET: Templates/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var template = await _context.Templates.Include(t => t.Layout).SingleOrDefaultAsync(m => m.TemplateId == id);
+      if (template == null)
+      {
+        return NotFound();
+      }
+
+      return View(template);
+    }
+
+    // GET: Templates/Create
+    public IActionResult Create()
+    {
+      ViewData["LayoutId"] = new SelectList(_context.Layouts, "LayoutId", "Name");
+      return View();
+    }
+
+    // POST: Templates/Create
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("TemplateId,FileContents,FileLocation,LayoutId,Name,IsForContentType")] Template template)
+    {
+      if (ModelState.IsValid)
+      {
+        _context.Add(template);
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
+      }
+      ViewData["LayoutId"] = new SelectList(_context.Layouts, "LayoutId", "Name", template.LayoutId);
+      return View(template);
+    }
+
+    // GET: Templates/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var template = await _context.Templates.SingleOrDefaultAsync(m => m.TemplateId == id);
+      // var template = await _context.Templates.Include(t => t.Layout).SingleOrDefaultAsync(m => m.TemplateId == id);
+      if (template == null)
+      {
+        return NotFound();
+      }
+      ViewData["LayoutId"] = new SelectList(_context.Layouts, "LayoutId", "Name", template.LayoutId);
+      return View(template);
+    }
+
+    // POST: Templates/Edit/5
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("TemplateId,FileContents,FileLocation,LayoutId,Name,IsForContentType")] Template template)
+    {
+      if (id != template.TemplateId)
+      {
+        return NotFound();
+      }
+
+      if (ModelState.IsValid)
+      {
+        try
+        {
+          _context.Update(template);
+          await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+          if (!TemplateExists(template.TemplateId))
+          {
+            return NotFound();
+          }
+          else
+          {
+            throw;
+          }
+        }
+        return RedirectToAction("Index");
+      }
+      ViewData["LayoutId"] = new SelectList(_context.Layouts, "LayoutId", "LayoutId", template.LayoutId);
+      return View(template);
+    }
+
+    // GET: Templates/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var template = await _context.Templates.Include(t => t.Layout).SingleOrDefaultAsync(m => m.TemplateId == id);
+      if (template == null)
+      {
+        return NotFound();
+      }
+
+      return View(template);
+    }
+
+    // POST: Templates/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+      var template = await _context.Templates.SingleOrDefaultAsync(m => m.TemplateId == id);
+      _context.Templates.Remove(template);
+      await _context.SaveChangesAsync();
+      return RedirectToAction("Index");
+    }
+
+    private bool TemplateExists(int id)
+    {
+      return _context.Templates.Any(e => e.TemplateId == id);
+    }
+  }
 }
